b86a0bdd362773a12ddea6aff9476427fe36ef79
hoangnd <hoangnd@miamioh.edu>
2024-09-23 23:18:36 -0400

    Put vertext positions into buffer

--- GameEngine/Shaders/vertexShader.glsl
+++ GameEngine/Shaders/vertexShader.glsl
@@ -5,13 +5,17 @@ layout(location = 100) uniform mat4 projectionMatrix;
 layout(location = 101) uniform mat4 viewMatrix;
 layout(location = 102) uniform mat4 modelMatrix;
 
+in vec4 position;
+
+/**
 // Declare a hard-coded array of positions
 const vec4 vertices[3] = vec4[3](vec4(-0.25, -0.25, 0.0, 1.0),
 								 vec4(0.25, -0.25, 0.0, 1.0),
                                  vec4(0.0, 0.25, 0.0, 1.0));
+*/
 
 void main()
 {
     // Calculate the position in clip coordinates
-     gl_Position = projectionMatrix * viewMatrix * modelMatrix * vertices[gl_VertexID];
+     gl_Position = projectionMatrix * viewMatrix * modelMatrix * position;
 }
--- GameEngine/TriangleMeshComponent.cpp
+++ GameEngine/TriangleMeshComponent.cpp
@@ -3,11 +3,34 @@
 
 void TriangleMeshComponent::buildMesh()
 {
+	// Submess define
 	SubMesh subMesh;
+
 	// Generate and bind a vertex array object.
 	// Store the identifier for the vertex array object in the subMesh
 	glGenVertexArrays(1, &subMesh.vao);
 	glBindVertexArray(subMesh.vao);
+
+	// Triangle positions
+	std::vector<vec4> positions;
+
+	positions.push_back(vec4(-0.25, -0.25, 0.0, 1.0));
+	positions.push_back(vec4(0.25, -0.25, 0.0, 1.0));
+	positions.push_back(vec4(0.0, 0.25, 0.0, 1.0));
+
+	// Create a buffer and load the vertex positions and colors into it.
+	// Store the identifier for the buffer in the subMesh.
+	glGenBuffers(1, &subMesh.vertexBuffer);
+	glBindBuffer(GL_ARRAY_BUFFER, subMesh.vertexBuffer);
+
+	// Copy the data into the buffer object
+	glBufferData(GL_ARRAY_BUFFER, positions.size() * sizeof(vec4), &positions[0], GL_STATIC_DRAW);
+
+	// Specify the location and data format of the vertex positions
+	glVertexAttribPointer(0, 4, GL_FLOAT, GL_FALSE, sizeof(vec4), 0);
+
+	// Enable the flow of the positions into the vertex shader
+	glEnableVertexAttribArray(0);
 
 	subMesh.primitiveMode = GL_TRIANGLES;
 	subMesh.renderMode = ORDERED;
